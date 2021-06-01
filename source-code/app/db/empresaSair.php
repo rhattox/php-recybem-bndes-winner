eff2ab2fb00e42b84bc783c1a12788eb59e94717
Bruno Souto <bcovies@gmail.com>
2021-06-01 18:00:34 -0300

    Removido infraestrutura

--- source-code/app/db/empresaSair.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['usuarioId'], $_SESSION['usuarioNome'], $_SESSION['usuarioEmail']);
-
-$_SESSION['msg'] = "Deslogado com sucesso";
-header("Location: index.html");
--- source-code/app/db/usuarioSair.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['usuarioId'], $_SESSION['usuarioNome'], $_SESSION['usuarioEmail'], $_SESSION['usuarioCpf']);
-$_SESSION['msg'] = "Deslogado com sucesso";
-header("Location: ../index.html");
